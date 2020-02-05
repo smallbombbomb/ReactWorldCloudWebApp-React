d046f6d574460d8391e2e43e2b8206873e7d7393
kimdonghyun <kdh8677@naver.com>
2020-02-05 23:37:59 +0900

    Text 추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './AppShell';
 import Home from './Home';
+import Texts from './Texts';
 
 class App extends React.Component {
 render() {
@@ -10,6 +11,7 @@ render() {
             <AppShell>
                 <div>
                     <Route exact path="/" component={Home}/>
+                    <Route exact path="/texts" component={Texts}/>
                 </div>
             </AppShell>
         </Router>
