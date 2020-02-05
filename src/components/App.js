b831a7d955db8662175b28dde600fc345b9097be
kimdonghyun <kdh8677@naver.com>
2020-02-05 23:50:58 +0900

    Route 수정

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import AppShell from './AppShell';
 import Home from './Home';
 import Texts from './Texts';
+import Words from './Words';
 
 class App extends React.Component {
 render() {
@@ -12,6 +13,7 @@ render() {
                 <div>
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/texts" component={Texts}/>
+                    <Route exact path="/words" component={Words}/>
                 </div>
             </AppShell>
         </Router>
