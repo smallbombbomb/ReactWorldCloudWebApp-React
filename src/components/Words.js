35c524ed2114ad667e77c10e1a0720c5e02473b0
dhkim <kdh8677@naver.com>
2020-02-14 17:53:44 +0900

    commit

--- src/components/Words.js
+++ src/components/Words.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const styles = theme => ({
     fab: {
@@ -25,15 +27,15 @@ class Words extends React.Component {
             word: '',
             weight: ''
         };
-    }
+    } ol
 
     _get() {
         fetch(`${databaseURL}/words.json`).then(res => {
-            if(res.status != 200) {
+            if (res.status != 200) {
                 throw new Error(res.statusText);
             }
             return res.json();
-        }).then(words => this.setState({words: words}));
+        }).then(words => this.setState({ words: words }));
     }
 
     _post(word) {
@@ -41,15 +43,28 @@ class Words extends React.Component {
             method: 'POST',
             body: JSON.stringify(word)
         }).then(res => {
-            if(res.status != 200) {
+            if (res.status != 200) {
                 throw new Error(res.statusText);
             }
             return res.json();
         }).then(data => {
             let nextState = this.state.words;
             nextState[data.name] = words;
-            this.setState({words: nextState});
+            this.setState({ words: nextState });
         });
+    }
+
+    _delete(id) {
+        return fetch(`${databaseURL}/words/${id}.json`, {
+            method: 'DELETE'
+        }).then(res => {
+            if (res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(() => {
+            let nextState = this.state.words;
+        })
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -59,6 +74,30 @@ class Words extends React.Component {
     componentDidMount() {
         this._get();
     }
+
+    handleDialogToggle = () => this.setState({
+        dialog: !this.state.dialog
+    })
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
+    handleSubmit = () => {
+        const word = {
+            word: this.state.word,
+            weight: this.state.weight
+        }
+        this.handleDialogToggle();
+        if (!word.word && !word.weight) {
+            return;
+        }
+        this._post(word);
+    }
+
+    //handle
 
     render() {
         return (
@@ -75,11 +114,11 @@ class Words extends React.Component {
                                     <Grid container>
                                         <Grid item xs={6}>
                                             <Typography variant="h5" component="h2">
-                                            {word.word}
+                                                {word.word}
                                             </Typography>
                                         </Grid>
-                                        <Grid item xs = {6}>
-                                            <Button variant="contained" color = "primary">삭제</Button>
+                                        <Grid item xs={6}>
+                                            <Button variant="contained" color="primary">삭제</Button>
                                         </Grid>
                                     </Grid>
                                 </CardContent>
@@ -87,6 +126,9 @@ class Words extends React.Component {
                         </div>
                     );
                 })}
+                <Fab color="primary" className={classes.fab}>
+                    <AddIcon />
+                </Fab>
             </div>
         );
     }
